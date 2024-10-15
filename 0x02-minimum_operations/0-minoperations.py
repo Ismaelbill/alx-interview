60d26be870409a868b8432ab4e15fb0846a1d033
ismael <spacingismael@gmail.com>
2024-10-15 21:50:44 +0100

    task minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """minOperations - func that calculates
+    the fewest number of operations needed
+    to result in exactly n H characters in the file."""
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    arr = []
+    for i in range(2, n):
+        val = (n // i) + i
+        arr.append(val)
+    return min(arr)
