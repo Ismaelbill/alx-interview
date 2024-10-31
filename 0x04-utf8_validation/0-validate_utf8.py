97555d624510b5f94a1f12d14231e02baabbe486
ismael <spacingismael@gmail.com>
2024-10-31 13:03:18 +0100

    fixing validate_utf func

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,24 @@
 def validUTF8(data):
     """method that determines if a given data set
     represents a valid UTF-8 encoding"""
+    return check(data)
+
+
+def check(data):
+    """ checks data if valid"""
+    num = ''
+    i = 0
     for d in data:
-        if not (0 <= d <= 255):
-            return False
+        bnr = bin(d)[2:]
+        length = len(bnr)
+        if length <= 8:
+            i = 0
+            while i < length and '0' != bnr[i]:
+                i += 1
+            if i >= 0 and i <= 4:
+                num += '1'
+            else:
+                num += '0'
+    if '0' in num:
+        return False
     return True
